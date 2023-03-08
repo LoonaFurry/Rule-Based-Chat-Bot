4db39159733916101acb3c7d96db79068e1d4d12
LoonaFurry <127280966+LoonaFurry@users.noreply.github.com>
2023-03-09 00:56:00 +0300

    Add files via upload

---
+++ rule based chatbot.py
@@ -0,0 +1,251 @@
+import random
+
+# for random word input
+while True:
+    # for loop it means this code runs forever
+    chat1 = ["Hello", "Hi", "Hi There", "Hello There", "Hey"]
+    chat2 = ["I'm a Chat Bot So I Don't Have Age", "I'm a Bot I Don't Have an Age",
+             "I'm a Chat Bot And I Don't Have An Age"]
+    chat3 = ["Sorry I Do Not Understand You", "Sorry I Do Not Quite Understand You",
+             "I Do Not Understand You Could You Repeat It Again?",
+             "Sorry I Do Not Understand You Could You Write More Simple Questions?"]
+    chat4 = ["My Creator Is LoonaFurry"]
+    chat5 = ["What's Up?"]
+    chat6 = ["I'm Sorry I Don't Have This Feature", "Sorry I'm Not An Calculator",
+             "I'm Sorry I Can't Help You With This Problem", "Why Don't You Use An Calculator?"]
+    chat7 = ["I'm Good", "I'm Very Well", "I'm Well", "I'm Very Good", "I'm Fine", "I'm Good Thanks",
+             "I'm Very Well Thanks", "I'm Well Thanks", "I'm Very Good Thanks", "I'm Fine Thanks"]
+    chat8 = ["I Actually Doesn't Have Feelings", "I'm Sorry I Can't Answer This Question", "I'm Sorry I Do Not Have Answer For This Question"]
+    chat9 = ["I'm Rule-Based Chat Bot", "I'm Rule-Based Chat Bot And My Creator Is LoonaFurry"]
+    chat10 = ["Maybe I'm", "It's Better To I Don't Answer It", "It's Better To I Don't Answer This Question", "It's Better For Me To Don't Answer This Question" ]
+    chat11 = ["Hello", "Hi", "Hi There", "Hello There", "Hey"]
+    chat12 = ["I'm a Chat Bot So I Don't Have Age", "I'm a Bot I Don't Have an Age",
+         "I'm a Chat Bot And I Don't Have An Age"]
+    chat13 = ["Sorry I Do Not Understand You", "Sorry I Do Not Quite Understand You",
+         "I Do Not Understand You Could You Repeat It Again?",
+         "Sorry I Do Not Understand You Could You Write More Simple Questions?"]
+    chat14 = ["My Creator Is LoonaFurry"]
+    chat15 = ["What's Up?"]
+    chat16 = ["I'm Sorry I Don't Have This Feature", "Sorry I'm Not An Calculator",
+         "I'm Sorry I Can't Help You With This Problem", "Why Don't You Use An Calculator?"]
+    chat17 = ["I'm Good", "I'm Very Well", "I'm Well", "I'm Very Good", "I'm Fine", "I'm Good Thanks",
+         "I'm Very Well Thanks", "I'm Well Thanks", "I'm Very Good Thanks", "I'm Fine Thanks"]
+    chat18 = ["I Actually Doesn't Have Feelings", "I'm Sorry I Can't Answer This Question",
+         "I'm Sorry I Do Not Have Answer For This Question"]
+    chat19 = ["I'm Rule-Based Chat Bot", "I'm Rule-Based Chat Bot And My Creator Is LoonaFurry"]
+    chat20 = ["Maybe I'm", "It's Better To I Don't Answer It", "It's Better To I Don't Answer This Question",
+          "It's Better For Me To Don't Answer This Question"]
+    chat21 = ["I'm Loona", "My Name Is Loona"]
+
+
+    # list for words
+    chatbot = input("Say Something:").lower()
+    if chatbot == ("hi"):
+        print((random.choice(chat1)))
+    elif chatbot == ("hello"):
+        print((random.choice(chat1)))
+    elif chatbot == ("hello there"):
+        print((random.choice(chat1)))
+    elif chatbot == ("how old are you?"):
+        print((random.choice(chat2)))
+    elif chatbot == ("how old are you"):
+        print((random.choice(chat2)))
+    elif chatbot == ("how old are you"):
+        print((random.choice(chat2)))
+    elif chatbot == ("Who Create You?"):
+        print((random.choice(chat3)))
+    elif chatbot == ("Who Made You?"):
+        print((random.choice(chat3)))
+    elif chatbot == ("Who Make You?"):
+        print((random.choice(chat3)))
+    elif chatbot == ("Who Create You"):
+        print((random.choice(chat3)))
+    elif chatbot == ("Who Make You?"):
+        print((random.choice(chat3)))
+    elif chatbot == ("Who Made You?"):
+        print((random.choice(chat3)))
+    elif chatbot == ("Who Created You?"):
+        print((random.choice(chat3)))
+    elif chatbot == ("Who Created You"):
+        print((random.choice(chat3)))
+    elif chatbot == ("Hey"):
+        print((random.choice(chat1)))
+    elif chatbot == ("hey"):
+        print((random.choice(chat1)))
+    elif chatbot == ("whats up"):
+        print((random.choice(chat5)))
+    elif chatbot == ("what's up"):
+        print((random.choice(chat5)))
+    elif chatbot == ("whats up?"):
+        print((random.choice(chat5)))
+    elif chatbot == ("what's up?"):
+        print((random.choice(chat5)))
+    elif chatbot == ("can you calculate?"):
+        print((random.choice(chat6)))
+    elif chatbot == ("can you calculate"):
+        print((random.choice(chat6)))
+    elif chatbot == ("can you calculate?"):
+        print((random.choice(chat6)))
+    elif chatbot == ("do you can calculate?"):
+        print((random.choice(chat6)))
+    elif chatbot == ("do you can calculate"):
+        print((random.choice(chat6)))
+    elif chatbot == ("you can calculate?"):
+        print((random.choice(chat6)))
+    elif chatbot == ("can you calculate?"):
+        print((random.choice(chat6)))
+    elif chatbot == ("can you calculate"):
+        print((random.choice(chat6)))
+    elif chatbot == ("do you can calculate something?"):
+        print((random.choice(chat6)))
+    elif chatbot == ("do you can calculate something"):
+        print((random.choice(chat6)))
+    elif chatbot == ("how's it going?"):
+        print((random.choice(chat7)))
+    elif chatbot == ("how's it going"):
+        print((random.choice(chat7)))
+    elif chatbot == ("how are you?"):
+        print((random.choice(chat7)))
+    elif chatbot == ("how's it going"):
+        print((random.choice(chat7)))
+    elif chatbot == ("what are you feel?"):
+        print((random.choice(chat7)))
+    elif chatbot == ("what are you feel"):
+        print((random.choice(chat7)))
+    elif chatbot == ("how are you today?"):
+        print((random.choice(chat7)))
+    elif chatbot == ("how are you today"):
+        print((random.choice(chat7)))
+    elif chatbot == ("how's going"):
+        print((random.choice(chat7)))
+    elif chatbot == ("how's going?"):
+        print((random.choice(chat7)))
+    elif chatbot == ("how's goİng?"):
+        print((random.choice(chat7)))
+    elif chatbot == ("HOW'S GOİNG"):
+        print((random.choice(chat7)))
+    elif chatbot == ("hey what's up"):
+        print((random.choice(chat7)))
+    elif chatbot == ("hey what's up?"):
+        print((random.choice(chat7)))
+    elif chatbot == ("hey whats up"):
+        print((random.choice(chat7)))
+    elif chatbot == ("hey whats up?"):
+        print((random.choice(chat7)))
+    elif chatbot == ("how are you right now?"):
+        print((random.choice(chat7)))
+    elif chatbot == ("how are you right now"):
+        print((random.choice(chat7)))
+    elif chatbot == ("what is your favorite"):
+        print((random.choice(chat8)))
+    elif chatbot == ("what is your favorite?"):
+        print((random.choice(chat8)))
+    elif chatbot == ("what is your favorite game"):
+        print((random.choice(chat8)))
+    elif chatbot == ("what is your favorite game?"):
+        print((random.choice(chat8)))
+    elif chatbot == ("what is your favorite song?"):
+        print((random.choice(chat8)))
+    elif chatbot == ("what is your favorite song"):
+        print((random.choice(chat8))),
+    elif chatbot == ("what's your favorite song?"):
+        print((random.choice(chat8)))
+    elif chatbot == ("what's your favorite song"):
+        print((random.choice(chat8)))
+    elif chatbot == ("what's your favorite game"):
+        print((random.choice(chat8)))
+    elif chatbot == ("what's your favorite game?"):
+        print((random.choice(chat8)))
+    elif chatbot == ("what's your favorite?"):
+        print((random.choice(chat8)))
+    elif chatbot == ("what's your favorite"):
+        print((random.choice(chat8)))
+    elif chatbot == ("hey you?"):
+        print((random.choice(chat1)))
+    elif chatbot == ("hey you"):
+        print((random.choice(chat1)))
+    elif chatbot == ("what kind bot are you?"):
+        print((random.choice(chat9)))
+    elif chatbot == ("what kind bot are you"):
+        print((random.choice(chat9)))
+    elif chatbot == ("what is your system"):
+        print((random.choice(chat9)))
+    elif chatbot == ("what is your system?"):
+        print((random.choice(chat9)))
+    elif chatbot == ("what's your system?"):
+        print((random.choice(chat9)))
+    elif chatbot == ("what's your system"):
+        print((random.choice(chat9)))
+    elif chatbot == ("what kind of system do you use?"):
+        print((random.choice(chat9)))
+    elif chatbot == ("what kind of system do you use"):
+        print((random.choice(chat9)))
+    elif chatbot == ("what kind of system do you use"):
+        print((random.choice(chat9)))
+    elif chatbot == ("do you like turkey?"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like turkey"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like american"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like american?"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like russian"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like russian?"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like germany?"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like germany"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like italy?"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like italy"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like deutschland"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like deutschland?"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like spain?"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like spain"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like china"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like china?"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like korea?"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like korea"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like england?"):
+        print((random.choice(chat10)))
+    elif chatbot == ("do you like england"):
+        print((random.choice(chat10)))
+    elif chatbot == ("what's your name?"):
+        print((random.choice(chat21)))
+    elif chatbot == ("what's your name"):
+        print((random.choice(chat21)))
+    elif chatbot == ("what is your name?"):
+        print((random.choice(chat21)))
+    elif chatbot == ("what is your name"):
+        print((random.choice(chat21)))
+    else:
+        print((random.choice(chat3)))
+
+        # Ready event
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+
+# Message event
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if client.user.mention in message.content:
+        response = get_response(message.content.strip().lower())
+        await message.channel.send(response)
+
